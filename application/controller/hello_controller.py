0ad5caf72ab8731f3541e49d00afb91b65563be0
Pedro Henrique Vasconcelos <pedroh.cpv@hotmail.com>
2020-06-05 19:02:36 -0300

    Mais curtidos
    Foi adicionado a função para exibir os vídeos mais curtidos na página inicial

--- application/controller/hello_controller.py
+++ application/controller/hello_controller.py
@@ -1,11 +1,18 @@
 from application import app
+from application import todos_os_videos
 from flask import render_template, request
 from application.model.dao.categoria_DAO import CategoriaDAO
 from application.model.entity.category import Category
+from application.model.entity.video import Video
 
 
 @app.route("/")
 def hello():
     categoria_dao = CategoriaDAO()
     category_list = categoria_dao.retornar_lista_videos()
-    return render_template("index.html", category_list = category_list)
+    
+    
+    lista_mais_curtidos_ordem = sorted(todos_os_videos, key=Video.get_todas_curtidas, reverse=True)
+    lista_mais_curtidos = [lista_mais_curtidos_ordem[0], lista_mais_curtidos_ordem[1]]
+
+    return render_template("index.html", category_list = category_list, lista_mais_curtidos = lista_mais_curtidos)
